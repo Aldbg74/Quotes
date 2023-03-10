3dd33c5e7d242c7ce00f3359ede804727e99845b
Alexis <63512507+Aldbg74@users.noreply.github.com>
2023-03-10 18:35:30 +0100

    Add files via upload

--- Quotes.py
+++ Quotes.py
@@ -4,9 +4,17 @@
 # 2023
 #
 
+
+# Thanks to my freinds who tell me some quotes
+# This is your reward
+# Thanks to you
+# 
+# Ben.G
+# Ossan.M
+
 import random
 import time
-quotes = random.randint(1, 10)
+quotes = random.randint(1, 19)
 
 while True: 
     print ("Quote of the day : ")
@@ -52,3 +60,40 @@ while True:
     if quotes == 10 :
         print("always remember, you're unique, just like everyone else.")
         break
+
+    if quotes == 11 :
+        print("The only way to move into the future is to forgive the past")
+        break
+
+    if quotes == 12 :
+        print("In life you have to take the good with the bad, smile with the sad, love what you got, and remember what you had. Always forgive, but never forget. Learn from mistakes, but never regret. People change, things go wrong, just remember life goes on.")
+        break
+
+    if quotes == 13 :
+        print("If you want to be happy, be.")
+        break
+
+    if quotes == 14 :
+        print("The best revenge is massive success.")
+        break
+
+    if quotes == 15 :
+        print("If you want to be horny, be horny")
+        break
+
+    if quotes == 16 :
+        print("Stress makes you bald, but it's stressful to avoid stress, so you end up stressed out anyway. In the end, there's nothing you can do.")
+        break
+
+    if quotes == 17:
+        print("Opportunities don't happen, you create them.")
+        break
+
+    if quotes == 18:
+        print("You time is limited, so don't waste it living someone else's life")
+        break
+
+    if quotes == 19:
+        print("Sometimes life is going to hit you in the head with a brick. Don't lose faith")
+        break
+
